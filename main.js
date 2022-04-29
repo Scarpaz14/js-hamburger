d14da7646a7285570b8480405ec2bbd2f5611ff5
Scarpaz14 <scarparofrancesco14@gmail.com>
2022-04-29 16:17:22 +0200

    aggiunto codice js per apertura link hamburger menu

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 
+// Quando clicco l'icona devono apparire i link
+
+const openMenu = document.getElementById("open-menu");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+
+openMenu.addEventListener('click',
+function() {
+    hamburgerMenu.style.display = "block";
+}
+);
+
+const closeMenu = document.getElementById("close-menu");
+closeMenu.addEventListener('click',
+function() {
+    hamburgerMenu.style.display = "none";
+}
+);
+
